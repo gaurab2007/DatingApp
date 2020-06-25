84df7c543805e87eef2cfb78252a481e05bd52c4
Gaurab Chatterjee <gaurab2007@gmail.com>
2020-06-25 20:30:54 +0530

    Added name for update

--- DatingApp.API/Dtos/UserForUpdateDto.cs
+++ DatingApp.API/Dtos/UserForUpdateDto.cs
@@ -2,6 +2,7 @@ namespace DatingApp.API.Dtos
 {
     public class UserForUpdateDto
     {
+         public string KnownAs { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
