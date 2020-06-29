959874421576be5ffc14bd2b1bd3f81b81256572
Gaurab Chatterjee <gaurab2007@gmail.com>
2020-06-29 10:58:35 +0530

    End of section 14

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,35 +18,59 @@ namespace DatingApp.API.Data
 
         public void Add<T>(T entity) where T : class
         {
-           _context.Add(entity);
+            _context.Add(entity);
         }
 
         public void Delete<T>(T entity) where T : class
         {
-           _context.Remove(entity);
+            _context.Remove(entity);
         }
 
         public async Task<Photo> GetPhoto(int id)
         {
-             var photo=await _context.Photos.FirstOrDefaultAsync(photo=>photo.Id==id);
+            var photo = await _context.Photos.FirstOrDefaultAsync(photo => photo.Id == id);
             return photo;
         }
 
         public async Task<User> GetUser(int id)
         {
-            var user= await _context.Users.Include(p=>p.Photos).FirstOrDefaultAsync(user=>user.Id==id);
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(user => user.Id == id);
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users= await _context.Users.Include(p=>p.Photos).ToListAsync();
-            return users;
+            var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
+            users = users.Where(u => u.Id != userParams.UserId);
+            users = users.Where(u => u.Gender == userParams.Gender);
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+            }
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
         {
-           return await _context.SaveChangesAsync()>0;
+            return await _context.SaveChangesAsync() > 0;
         }
 
         public async Task<Photo> GetMainPhotoForUser(int userId)
